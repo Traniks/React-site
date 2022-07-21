99bd98bc8ac75450626e468a638db4490305371e
Traniks <andert2017@gmail.com>
2022-07-21 21:02:44 +0300

    126.1 сайт

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css"
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудноков в CumПании</h1>
+            <h2>Общее число slaves: </h2>
+            <h2>Fisting получат: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
